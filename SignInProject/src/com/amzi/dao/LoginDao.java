6c1875399d83268eb8c26cb13b2a19732a170399
KhaMai <KhaMai@KHAMAI-LAPTOP>
2018-02-10 14:36:44 -0500

    l

--- SignInProject/src/com/amzi/dao/LoginDao.java
+++ SignInProject/src/com/amzi/dao/LoginDao.java
@@ -17,7 +17,7 @@ public class LoginDao {
         String dbName = "form";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "jjj";
+        String password = "22";
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager
